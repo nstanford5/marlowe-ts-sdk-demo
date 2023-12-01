7fb4b1067247fe86bc72ad60940cbaae09244adc
Nick Stanford <nstanford5@yahoo.com>
2023-12-01 10:28:48 -0700

    first, works

--- src/App.js
+++ src/App.js
@@ -1,6 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
 
+import * as wallet from '@marlowe.io/wallet';
+import { mkRuntimeLifecycle } from '@marlowe.io/runtime-lifecycle/browser';
+
+const installedWalletExtensions = wallet.getInstalledWalletExtensions();
+console.log(installedWalletExtensions);
+
+const walletName = installedWalletExtensions[0].name;
+console.log(`Available Browser Wallet Extensions: ${walletName}`);
+const runtimeURL = 'https://marlowe-runtime-preprod-web.demo.scdev.aws.iohkdev.io';
+
+const runtimeLifecycle = await mkRuntimeLifecycle({
+  walletName: walletName,
+  runtimeURL: runtimeURL,
+});
+
+console.log('Connected to runtime...');
+
+const userWallet = await wallet.mkBrowserWallet(walletName);
+const lovelaces = await userWallet.getLovelaces();
+
+console.log(`This user has ${lovelaces} Lovelace in their wallet`);
+
+
+await runtimeLifecycle.contracts.createContract({
+  contract: "close",
+});
+
+console.log(`Contract success!`);
+
 function App() {
   return (
     <div className="App">
